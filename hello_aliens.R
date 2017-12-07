7e1b9dd48c79d50169570b481814d4ae41a3b85b
theoyannekis <33228851+theoyannekis@users.noreply.github.com>
2017-12-07 15:33:38 -0500

    Create hello_aliens.R

---
+++ hello_aliens.R
@@ -0,0 +1,8 @@
+cat("Hello aliens, I hope everything is going well on your planet. I'm wondering 
+    how the language barrier will apply here, but we're both smart species so 
+    I'm sure we can figure it out. As a race we're doing alright down here. We
+    have some trust issues but overall we're doing our best to figure our 
+    purpose out. How are you up there? I'm curious to know how your world works
+    and how your society operates and how you live your everyday life looks.
+    What do you eat? Do you listen to music? We should have a full conversation
+    sometime. Best of luck.")
